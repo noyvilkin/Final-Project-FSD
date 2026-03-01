7d32e9a285817889da4c288d7907a0d477ba44f0
Vered-Ben-David <vered054819@gmail.com>
2026-03-01 18:35:50 +0200

    Add global auth context and protected routing

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,12 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 import MyProfile from "../pages/MyProfile";
 
-export default function AppRouter() {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/profile" replace />} />
-      <Route path="/profile" element={<MyProfile />} />
-      <Route path="*" element={<div>404</div>} />
-    </Routes>
-  );
-}
+<Route
+  path="/profile"
+  element={
+    <ProtectedRoute>
+      <MyProfile />
+    </ProtectedRoute>
+  }
+/>
