72b2cc9e7b43bb8826bbb64b73090c2c5ed3ac0b
Vered-Ben-David <vered054819@gmail.com>
2026-03-06 12:41:02 +0200

    Fix AppRouter default export and temporarily disable ProtectedRoute to allow UI to run

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,11 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import MyProfile from "../pages/MyProfile";
 
-<Route
-  path="/profile"
-  element={
-    <ProtectedRoute>
-      <MyProfile />
-    </ProtectedRoute>
-  }
-/>
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/profile" replace />} />
+
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <MyProfile />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<div>404</div>} />
+    </Routes>
+  );
+}
